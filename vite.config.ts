627690418d7e6df57576e1bfa74238f871057626
Courtney Gooch <courtneygooch@Courtneys-iMac.local>
2026-03-11 11:15:28 -0700

    Push Distribution to build and deploy static.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NanuPika/',
+  base: '/',
   plugins: [vue()],
   build: {
-    outDir: './',
-    emptyOutDir: false,
+    outDir: 'dist',
   }
 })
